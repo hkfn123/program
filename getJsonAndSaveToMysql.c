79b805c42d3900807da6faf806c676b286b22761
steven <hkfn123@163.com>
2013-12-03 21:03:00 -0500

    first commit

---
+++ getJsonAndSaveToMysql.c
@@ -0,0 +1,62 @@
+#include <curl/curl.h>
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
+
+
+struct MemoryStruct{
+	char* memory;
+	size_t size;
+};
+
+
+static size_t WriteMemoryCallback(void* contents,size_t size,size_t nmemb,void* userp)
+{
+	size_t realsize = size*nmemb;
+	struct MemoryStruct *mem = (struct MemoryStruct *)userp;
+	
+	mem->memory = realloc(mem->memory,mem->size+realsize+1);
+	if(mem->memory == NULL)
+	{
+		printf("not enough memory (realloc return NULL)\n");
+		return 0;
+	}
+	memcpy(&(mem->memory[mem->size]),contents,realsize);
+	mem->size += realsize;
+	mem->memory[mem->size] = 0;
+	
+	return realsize;
+}
+
+
+char* getURLContentfromURLString(const char* theURL)
+{
+	CURL* curl;
+	CURLcode res;
+
+ 	struct MemoryStruct chunk;
+	chunk.memory = malloc(1);
+	chunk.size = 0;	
+	
+	curl_global_init(CURL_GLOBAL_ALL);
+
+	curl = curl_easy_init();
+	if(curl == NULL)
+	{
+		fprintf(stderr,"curl init error!\n");
+		return NULL;
+	}
+	curl_easy_setopt(curl,CURLOPT_URL,theURL);
+	curl_easy_setopt(curl,CURLOPT_TIMEOUT,3);
+	curl_easy_setopt(curl,CURLOPT_WRITEDATA,(void*)&chunk);
+	curl_easy_setopt(curl,CURLOPT_WRITEFUNCTION,WriteMemoryCallback);	
+
+	res = curl_easy_perform(curl);
+	if(CURLE_OK != res){
+		printf("curl_easy_perform() error:%s\n",curl_easy_strerror(res));
+		return NULL;
+	}
+	curl_easy_cleanup(curl);
+	curl_global_cleanup();
+	return chunk.memory;
+}
---
+++ getJsonAndSaveToMysql.h
@@ -0,0 +1,5 @@
+#ifndef __getJsonAndSaveToMysql_h
+#define __getJsonAndSaveToMysql_h
+char* getURLContentfromURLString(const char* theURL);
+#endif
+
